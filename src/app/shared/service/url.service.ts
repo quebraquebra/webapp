65e8af2197e4eb4d213a362b1bcd4016b425090e
Fagner Lima <contato@fagnerlima.pro.br>
2017-07-09 21:06:31 -0300

    Atualiza o URLService

--- src/app/shared/service/url.service.ts
+++ src/app/shared/service/url.service.ts
@@ -1,12 +1,14 @@
 import { Injectable } from '@angular/core';
 
-// const urlApi = 'http://api.quebraquebra.fagnerlima.pro.br';
-const apiUrl = 'http://api.quebraquebra.dev';
+import { environment } from './../../../environments/environment';
+
+const apiUrlProd = 'http://api.quebraquebra.fagnerlima.pro.br';
+const apiUrlDev = 'http://api.quebraquebra.dev';
 
 @Injectable()
 export class UrlService {
 
   public static mountApiUrl(route: string): string {
-    return `${apiUrl}/${route}`;
+    return `${environment.production ? apiUrlProd : apiUrlDev}/${route}`;
   }
 }
