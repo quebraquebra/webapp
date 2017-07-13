d7dbe9a939f49f386714334e51391f0e7e58fdda
Fagner Lima <contato@fagnerlima.pro.br>
2017-07-12 23:31:58 -0300

    Refatora filtros do FolhaCamaraComponent

--- src/app/folha-camara/folha-camara.component.ts
+++ src/app/folha-camara/folha-camara.component.ts
@@ -3,6 +3,7 @@ import { Component, Type } from '@angular/core';
 import { FolhaCamaraService, FolhaCamara, FolhaCamaraFilter } from '.';
 import { PerfilComponent } from './perfil';
 import { GenericTableConfig } from '../shared/generic-table';
+import { quantidadesRegistros, anos, meses } from '../shared/util/filter';
 
 @Component({
   selector: 'app-folha-camara',
@@ -11,11 +12,19 @@ import { GenericTableConfig } from '../shared/generic-table';
 })
 export class FolhaCamaraComponent {
 
+  public anos: Array<number>;
+  public meses: { [valor: number]: string};
+  public quantidadesRegistros: Array<number>;
   public filter: FolhaCamaraFilter;
   public gtConfig: GenericTableConfig;
   public gtRowComponent: Type<any> = PerfilComponent;
+  public object = Object;
 
   public constructor(public folhaCamaraService: FolhaCamaraService) {
+    this.anos = anos;
+    this.meses = meses;
+    this.quantidadesRegistros = quantidadesRegistros;
+
     const date: Date = new Date();
     this.filter = {
       limit: 10,
---
+++ src/app/shared/util/filter.ts
@@ -0,0 +1,16 @@
+export const quantidadesRegistros = [10, 20, 30, 50];
+export const anos = [2017, 2016, 2015, 2014, 2013, 2012, 2011];
+export const meses: { [mes: number]: string} = {
+  1: 'Janeiro',
+  2: 'Fevereiro',
+  3: 'Março',
+  4: 'Abril',
+  5: 'Maio',
+  7: 'Julho',
+  6: 'Junho',
+  8: 'Agosto',
+  9: 'Setembro',
+  10: 'Outubro',
+  11: 'Novembro',
+  12: 'Dezembro'
+};
